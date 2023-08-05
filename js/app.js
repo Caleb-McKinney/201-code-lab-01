2d2d88b64ff1517353952d54e116228bc014b8c8
Caleb-McKinney <McKinney.Caleb@gmail.com>
2023-08-05 18:09:03 -0500

    another JS typo fix

--- js/app.js
+++ js/app.js
@@ -5,12 +5,12 @@ let codeSchool = prompt("Do you know CSS?");
 if (codeSchool == "yes") alert("You have style!");
 else alert("That is ok");
 let muffinMan = prompt("Do you know the muffin man?");
-if (muffinMan == "yes") alert("I bet his muffins are better than the Rock's")
+if (muffinMan == "yes") alert("I bet his muffins are better than the Rock's");
 else alert("I'd sure like to meet him");
 let theFair = prompt("Are you going to Scarborough fair?");
-if (theFair == "yes") alert("I'll see you there!")
-else alert("Yeah sounds lame.");
+if (theFair == "yes") alert("I'll see you there!");
+else alert("Yeah sounds lame");
 let funnyPage = prompt("Did this page make you laugh?");
-if (funnyPage == "yes") alert("my work here is finished")
+if (funnyPage == "yes") alert("my work here is finished");
 else alert("try again");
 console.log(greeting + "," + favPlayer + "He was a great player");
