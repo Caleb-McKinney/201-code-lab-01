8d8981d92a0161156baf8cb56e9bc9269221cc65
Caleb-McKinney <McKinney.Caleb@gmail.com>
2023-08-05 17:58:42 -0500

    lab-02 updates- added css and js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let greeting = prompt("Can you smell what the rock is cookin'");
+if (greeting == "yes") alert("Hes a chef now?!");
+else alert("Me either");
+let codeSchool = prompt("Do you know CSS?");
+if (codeSchool == "yes") alert("You have style!");
+else alert("That is ok");
+let muffinMan = prompt("Do you know the muffin man?");
+if (muffinMan == "yes") alert("I bet his muffins are better than the Rock's")
+else alert("I'd sure like to meet him");
+let theFair = prompt("Are you going to Scarborough fair?");
+if (theFair = "yes") alert("I'll see you there!")
+else alert("Yeah sounds lame.");
+let funnyPage = prompt("Did this page make you laugh?");
+if (funnyPage == "yes") alert("my work here is finished")
+else alert("try again");
+console.log(greeting + "," + favPlayer + "He was a great player");
